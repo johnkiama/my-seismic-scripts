da2f716e266ec6f1ee0418d312047c3bd0e262fe
johnkiama <36688543+johnkiama@users.noreply.github.com>
2022-06-07 11:28:07 +0300

    Identify changes done to a file

---
+++ my_file_monitor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+modification_year=`date -r user-file |awk '{print $6}'`
+modification_time=`date -r user-file |awk '{print $4}'`
+
+
+
+if [ ! -f $(readlink -f logmd5.txt) ]; then
+#  touch logmd5.txt
+  md5sum user-file | awk '{print $1}'>logmd5.txt
+fi
+
+FILE1=$(md5sum user-file | awk '{print $1}')
+FILE2=`cat logmd5.txt`
+#FILE2=`cat logmd5.txt | tail -1` 
+if [[ $FILE1 == $FILE2 ]]
+then
+        echo "File not changed"
+#        md5sum user-file | awk '{print $1}'>>logmd5.txt
+else     
+        echo "File changed on $modification_year at $modification_time" >>log-user-changes.txt
+        md5sum user-file | awk '{print $1}'>logmd5.txt
+
+fi
+
